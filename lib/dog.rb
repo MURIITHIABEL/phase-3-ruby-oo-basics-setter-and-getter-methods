87558dc9a1a6939314dd6b882a533157d4882d67
Abel Muriithi <abelmuriithi04@gmail.com>
2023-02-09 15:25:48 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
 
-end
+    def name=(name)
+      @name = name
+    end
+  
+    def breed=(breed)
+        @breed = breed
+      end
+   
+
+    
+    def breed
+        @breed
+      end
+
+    def name
+      @name
+    end
+  
+  end
